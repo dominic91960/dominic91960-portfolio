b7eeb0a41c629dc0ff003cef190ebf448e6b4bba
Dominic Brian <dominic91960@gmail.com>
2025-07-06 23:09:48 +0530

    fix(home-page): project tech stack changes

--- components/home-page/projects/ProjectTechStack.tsx
+++ components/home-page/projects/ProjectTechStack.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import React from "react";
 
 type ProjectTechStackProps = {
@@ -9,16 +8,17 @@ type ProjectTechStackProps = {
 };
 
 const ProjectTechStack: React.FC<ProjectTechStackProps> = ({ data }) => {
+  const colCount = data.length > 0 ? data.length : 4;
   return (
     <div className="text-foreground border-foreground/30 col-span-2 flex items-center gap-[3em] rounded-[calc(var(--radius)*0.8)] border p-[2em]">
       <h3 className="shrink-0 self-center text-[22px] font-bold sm:text-[24px] md:text-[27px] lg:text-[29px] xl:text-[31px] 2xl:text-[32px]">
         Tech <br /> Stack
       </h3>
       <div
-        className={cn(
-          "grid h-[60px] w-full grow grid-cols-4 gap-[2em]",
-          `grid-cols-${data.length}`,
-        )}
+        className="grid h-[60px] w-full grow gap-[2em]"
+        style={{
+          gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
+        }}
       >
         {data.map(({ id, Icon }) => (
           <Icon key={id} className="size-full opacity-30" />
