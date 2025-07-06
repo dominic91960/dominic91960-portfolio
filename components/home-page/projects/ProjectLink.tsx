e10c9a9f5f7a1d40f9c848a7c8be9f9d742651cc
Dominic Brian <dominic91960@gmail.com>
2025-07-06 23:05:45 +0530

    fix(home-page): minor bug fixes

--- components/home-page/projects/ProjectLink.tsx
+++ components/home-page/projects/ProjectLink.tsx
@@ -6,7 +6,7 @@ import { FiExternalLink } from "react-icons/fi";
 const ProjectLink: React.FC<{ data: string }> = ({ data }) => {
   return (
     <div className="text-foreground border-foreground/30 flex items-center justify-center rounded-[calc(var(--radius)*0.8)] border p-[2em]">
-      <Link href={data} target="_blank" rel="noopener noreferrer" className="">
+      <Link href={data} target="_blank" rel="noopener noreferrer">
         <button className="flex items-center gap-[0.5em] text-[22px] font-bold transition-opacity duration-300 hover:opacity-80 focus:opacity-80 sm:text-[24px] md:text-[27px] lg:text-[29px] xl:text-[31px] 2xl:text-[32px]">
           Link To Site
           <FiExternalLink />
--- components/home-page/projects/ProjectTechStack.tsx
+++ components/home-page/projects/ProjectTechStack.tsx
@@ -9,14 +9,17 @@ type ProjectTechStackProps = {
 };
 
 const ProjectTechStack: React.FC<ProjectTechStackProps> = ({ data }) => {
-  const colCount = `grid-cols-${data.length}`;
-
   return (
     <div className="text-foreground border-foreground/30 col-span-2 flex items-center gap-[3em] rounded-[calc(var(--radius)*0.8)] border p-[2em]">
       <h3 className="shrink-0 self-center text-[22px] font-bold sm:text-[24px] md:text-[27px] lg:text-[29px] xl:text-[31px] 2xl:text-[32px]">
         Tech <br /> Stack
       </h3>
-      <div className={cn("grid h-[60px] grow gap-[2em]", colCount)}>
+      <div
+        className={cn(
+          "grid h-[60px] w-full grow grid-cols-4 gap-[2em]",
+          `grid-cols-${data.length}`,
+        )}
+      >
         {data.map(({ id, Icon }) => (
           <Icon key={id} className="size-full opacity-30" />
         ))}
